4357ff08b0794fa9f9378f9b99473cb8fd77bb81
Kashinka <azalena@yandex.ru>
2022-10-23 17:32:13 +0300

    Added function

---
+++ theme.js
@@ -0,0 +1,23 @@
+function hangeBackgroundToWhite(color) {
+
+    document.body.style.background = color;
+    
+}
+
+window.addEventListener("load",function() { changeBackgroundToWhite('white') });
+
+function hangeBackgroundToBlue(color) {
+
+    document.body.style.background = color;
+
+}
+
+window.addEventListener("load",function() { changeBackgroundToBlue('blue') });
+
+function hangeBackgroundToBlack(color) {
+
+    document.body.style.background = color;
+
+}
+
+window.addEventListener("load",function() { changeBackgroundToBlack('black') });
